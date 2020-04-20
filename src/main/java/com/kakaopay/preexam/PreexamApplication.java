a986e1592d564f3789e95372d05e485a6232cb6f
Won Seok Cho <minov@Won-Seok-Choui-MacBookPro.local>
2020-04-20 14:45:55 +0900

    Initial commit

---
+++ src/main/java/com/kakaopay/preexam/PreexamApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.preexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreexamApplication.class, args);
+    }
+
+}
