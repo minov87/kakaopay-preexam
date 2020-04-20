7233a425fa3d9d9eea81e627e1f3098e7547a339
minov87 <minov87@gmail.com>
2020-04-20 23:41:16 +0900

    프로젝트 기본 설정 수정

--- src/main/java/com/kakaopay/preexam/PreexamApplication.java
+++ src/main/java/com/kakaopay/preexam/PreexamApplication.java
@@ -2,12 +2,12 @@ package com.kakaopay.preexam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement(proxyTargetClass=true)
 public class PreexamApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PreexamApplication.class, args);
     }
-
 }
