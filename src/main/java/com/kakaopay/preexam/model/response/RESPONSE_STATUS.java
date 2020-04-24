886470aed9246c2e40b8165a8e61fa4c47beee27
minov87 <minov87@gmail.com>
2020-04-24 18:43:54 +0900

    TOKEN_VERIFY_FAIL_ACCOUNT_NOT_EXIST 추가

--- src/main/java/com/kakaopay/preexam/model/response/RESPONSE_STATUS.java
+++ src/main/java/com/kakaopay/preexam/model/response/RESPONSE_STATUS.java
@@ -14,20 +14,21 @@ public enum RESPONSE_STATUS {
 
     // Token
     TOKEN_VERIFY_FAIL(206, "Token verify fail"),
-    TOKEN_EXPIRED(207, "Token expired"),
-    TOKEN_EMPTY(208, "Token Empty"),
+    TOKEN_VERIFY_FAIL_ACCOUNT_NOT_EXIST(207, "Token verify fail"),
+    TOKEN_EXPIRED(208, "Token expired"),
+    TOKEN_EMPTY(209, "Token Empty"),
 
     // Account
-    ACCOUNT_ALREADY_EXIST(209, "Account already exist"),
-    ACCOUNT_NOT_EXIST(210, "Account not exist"),
-    ACCOUNT_WRONG_PASSWORD(211, "Account password is wrong"),
+    ACCOUNT_ALREADY_EXIST(210, "Account already exist"),
+    ACCOUNT_NOT_EXIST(211, "Account not exist"),
+    ACCOUNT_WRONG_PASSWORD(212, "Account password is wrong"),
 
     // Coupon
-    COUPON_MAKE_FAIL(212, "Coupon make fail"),
-    COUPON_NOT_ENOUGH(213, "Coupon not enough"),
-    COUPON_NOT_EXIST(214, "Coupon not exist"),
-    COUPON_ALREADY_USED(215, "Coupon already used"),
-    COUPON_EXPIRED(216, "Coupon expired");
+    COUPON_MAKE_FAIL(213, "Coupon make fail"),
+    COUPON_NOT_ENOUGH(214, "Coupon not enough"),
+    COUPON_NOT_EXIST(215, "Coupon not exist"),
+    COUPON_ALREADY_USED(216, "Coupon already used"),
+    COUPON_EXPIRED(217, "Coupon expired");
 
     private int code;
     private String message;
